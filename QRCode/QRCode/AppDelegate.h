0a1027f926d48802e03ee5ce20c6950b3a2a0357
边雷 <775884216@qq.com>
2017-01-13 14:44:22 +0800

    项目准备

---
+++ QRCode/QRCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QRCode
+//
+//  Created by 边雷 on 17/1/13.
+//  Copyright © 2017年 Mac-b. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QRCode/QRCode/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  QRCode
+//
+//  Created by 边雷 on 17/1/13.
+//  Copyright © 2017年 Mac-b. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+
+
+
+@end
